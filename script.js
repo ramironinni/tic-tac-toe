814cd002746f12c482dc6bfcd6766174da8578d0
ramironinni <ramironinni@gmail.com>
2021-06-16 14:50:03 -0300

    Add styles

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ window.addEventListener("DOMContentLoaded", () => {
         for (let i = 0; i < 9; i++) {
             db.push(null);
         }
-        return { db };
+
+        const div = document.getElementById("grid");
+
+        return { db, div };
     })();
 
     const players = (() => {
-        const player = (alias, symbol) => {
-            return { alias, symbol };
+        const player = (symbol) => {
+            return { symbol };
         };
 
-        const x = player("X", "X"); // TODO ask name to player A
-        const o = player("O", "O"); // TODO ask name to player B
+        const x = player("X"); // TODO ask name to player A
+        const o = player("O"); // TODO ask name to player B
 
         let current = x;
 
@@ -29,19 +32,60 @@ window.addEventListener("DOMContentLoaded", () => {
                 return;
             }
 
-            grid.db[position] = players.current;
-            displayMove(e);
-
+            grid.db[position] = players.current.symbol;
             console.log(grid.db);
+        }
+
+        function checkWinner() {
+            // console.table(grid.db);
+            let winner = null;
+
+            const xMoves = grid.db.reduce((a, e, i) => {
+                if (e === "X") a.push(i);
+                return a;
+            }, []);
+
+            const oMoves = grid.db.reduce((a, e, i) => {
+                if (e === "O") a.push(i);
+                return a;
+            }, []);
+
+            // console.log(xMoves);
+            // console.log(oMoves);
+
+            gameOverResults.forEach((result) => {
+                if (
+                    xMoves.indexOf(result[0]) > -1 &&
+                    xMoves.indexOf(result[1]) > -1 &&
+                    xMoves.indexOf(result[2]) > -1
+                ) {
+                    winner = players.x;
+                }
+
+                if (
+                    oMoves.indexOf(result[0]) > -1 &&
+                    oMoves.indexOf(result[1]) > -1 &&
+                    oMoves.indexOf(result[2]) > -1
+                ) {
+                    winner = players.o;
+                }
+            });
+
+            return winner;
         }
 
         function displayMove(e) {
             const currentPosition = e.target;
+
+            if (currentPosition.innerText) {
+                return;
+            }
+
             currentPosition.innerText = players.current.symbol;
             currentPosition.classList.add("taken");
 
-            const userClass = players.current.symbol === "X" ? "xs" : "os";
-            currentPosition.classList.add(userClass);
+            const playerClass = players.current.symbol === "X" ? "xs" : "os";
+            currentPosition.classList.add(playerClass);
         }
 
         function togglePlayer() {
@@ -49,13 +93,40 @@ window.addEventListener("DOMContentLoaded", () => {
                 players.current === players.x ? players.o : players.x;
         }
 
+        function endGame(winner) {
+            // grid.div.classList.add("hidden");
+            const winnerDiv = document.getElementById("winner");
+            winnerDiv.innerText = `Winner: ${winner.symbol}`;
+            // winnerDiv.classList.remove("hidden");
+            console.log(winner);
+        }
+
         function processMove(e) {
             saveMove(e);
+            displayMove(e);
+
+            const winner = checkWinner();
+            if (winner) {
+                endGame(winner);
+                return;
+            }
+
             togglePlayer();
         }
 
         const gridDiv = document.getElementById("grid");
         gridDiv.addEventListener("click", processMove);
+
+        const gameOverResults = [
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
     };
 
     function newGame() {
@@ -68,11 +139,9 @@ window.addEventListener("DOMContentLoaded", () => {
             // const playerXAlias = document.getElementById("player-x-alias");
             // const playerOAlias = document.getElementById("player-o-alias");
 
-            const form = e.currentTarget;
-            form.classList.add("hidden");
+            formStartGame.classList.add("hidden");
 
-            const gridDiv = document.getElementById("grid");
-            gridDiv.classList.remove("hidden");
+            grid.div.classList.remove("hidden");
 
             playGame();
         }
